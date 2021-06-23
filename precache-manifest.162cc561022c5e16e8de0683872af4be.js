c81b4f8cd0ce599aeca9c436be374a7bae4ccb35
whh <544956639@qq.com>
2021-06-23 12:34:11 +0800

    deploy

---
+++ precache-manifest.162cc561022c5e16e8de0683872af4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666eddb7d0fe83966ee0",
+    "url": "/haha-accounting-website/css/app.799e57e6.css"
+  },
+  {
+    "revision": "cdaf5d7e1f9a04439cadca23ae4dbe2e",
+    "url": "/haha-accounting-website/index.html"
+  },
+  {
+    "revision": "666eddb7d0fe83966ee0",
+    "url": "/haha-accounting-website/js/app.1ea6da4a.js"
+  },
+  {
+    "revision": "f196627ea9fbca6448c7",
+    "url": "/haha-accounting-website/js/chunk-vendors.8b66c80f.js"
+  },
+  {
+    "revision": "e09939e8bdb3bc07e427e0d8dbf41c91",
+    "url": "/haha-accounting-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/haha-accounting-website/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/haha-accounting-website/precache-manifest.162cc561022c5e16e8de0683872af4be.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "accounting"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
